f3b727885c79b159aa0d492a31b48e0f036aed4c
theatre <theatre>
2023-07-19 17:27:40 +0300

    ширина столбцов

--- src/app/components/account/overview/overview.component.ts
+++ src/app/components/account/overview/overview.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class OverviewComponent {
 
+
+  str1: string = "";
+  str2: string = "";
+  str3: string = "";
+
+
+  ngOnInit() {
+    this.clickWigth();
+  }
+
+  clickWigth() {
+    const col1 = <HTMLTableCellElement>document.getElementById('col1');
+    const col2 = <HTMLTableCellElement>document.getElementById('col2');
+    const col3 = <HTMLTableCellElement>document.getElementById('col3');
+
+    
+    this.str1 = "Столбец1: "+col1.clientWidth;
+    this.str2 = "Столбец2: "+col2.clientWidth;
+    this.str3 = "Столбец3: "+col3.clientWidth;
+
+    }
+
 }
