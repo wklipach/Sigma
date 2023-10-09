933ba18b448c3716055cb401c225d5ead5f48291
theatre <theatre>
2023-10-09 15:32:02 +0300

    dragdrop2

--- src/app/components/protected_objects/drag-drop-staff2/drag-drop-staff2.component.ts
+++ src/app/components/protected_objects/drag-drop-staff2/drag-drop-staff2.component.ts
@@ -46,30 +46,6 @@ export class DragDropStaff2Component {
   dd_array: IObj[] = [];
   dd_result: IDDResult2[] = [];
 
-  data = [
-    {
-      name: "Иван Иванов",
-      profileUrl : "assets/img/avatar.png",
-      area: "Дом1",
-      id: 1,
-      parentId: null
-    },
-    {
-      name: "Петр Петров",
-      profileUrl : "assets/img/avatar.png",
-      area: "Дом2",
-      id: 2,
-      parentId: 1
-    },
-    {
-      name: "Федор Федоров",
-      profileUrl : "assets/img/avatar.png",
-      area: "Дом3",
-      id: 3,
-      parentId: 1 
-    }
-  ];
-
 
   constructor(private dds: DragDropStaffService, private gr: GlobalRef, private route: ActivatedRoute) {}
 
@@ -123,27 +99,14 @@ export class DragDropStaff2Component {
                   if (!val.photo_name) val.photo_name="/assets/img/usernull.jpg"; else val.photo_name= this.gr.sUrlAvatarGlobal+ val.photo_name;
                   if (!val.rank) val.rank = "";
 
-                  this.dd_result.push({id: val.id, name: val.fio, address: "", photo_name: val.photo_name, rank: val.rank, parentId: val.id_object*(-1) });
+                  this.dd_result.push({id: val.id, name: val.fio, address: "", photo_name: val.photo_name, rank: val.rank, parentId: val.id_object*(-1), id_staff: val.id_staff });
 
                 });
-
-                //console.log('i=', i, 'dataArray[i]=', dataArray[i]);
-
-                //id: number;
-                //name: string;
-                //address: string;
-                //photo_name: string;
-                //rank: string;
-                //parentId?: number;                
-
-            
-                //this.dd_result.push( {name: this.dd_array[i].name, id_object: this.dd_array[i].id, address: this.dd_array[i].address, items: itemRes});
 
           }
 
           //console.log('this.dd_result=', this.dd_result);
           this.dd_result = [...this.dd_result];
-       
 
     });
 
@@ -153,44 +116,71 @@ export class DragDropStaff2Component {
 
 
   dragStart(event: any) {
-
-    event.dataTransfer.setData("text/plain", event.target.id);
-
-
     if (event.originalTarget) {
       if (event.originalTarget.attributes) {
-         console.log('dragStart=', event.originalTarget.attributes);
+         console.log('dragStart tree_id=', event.originalTarget.attributes.tree_id.value);
+         this.beginTree = event.originalTarget.attributes.tree_id.value;
       }
     }
   }
 
   dragEnd(event: any) {
-    console.log('dragEnd');
+
+
+    if (event.originalTarget) {
+      if (event.originalTarget.attributes) {
+        if (event.originalTarget.attributes.tree_id) {
+            this.endTree = event.originalTarget.attributes.tree_id.value;
+            console.log('dragEnd this.beginTree = ', this.beginTree);
+            console.log('dragEnd this.endTree = ', this.endTree);
+
+            this.moveElement(this.beginTree, this.endTree);
+
+        }
+      }
+    }
+
   }
 
   dragOver(event: any) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'point'; // ставим тип курсора
-
+     event.preventDefault();
   }
 
 
   moveElement(beginId: number, endId: number) {
-    this.data.forEach ( treeElement => {
+
+    // если это корень выходим 
+    if  (endId == 0) {
+      return;
+    }
+
+    // есди это человек ищем код объекта где он служит
+    if  (this.endTree > 0) {
+      let elem = this.dd_result.find( elem => elem.id.toString() ==  this.endTree.toString() )
+      if (elem && elem.parentId) {
+        endId = elem.parentId;
+      }
+    }
+
+    // если по прежнему не нашли выходим
+    if  (endId >= 0) {
+      return;
+    }
+
+    this.dd_result.forEach ( treeElement => {
       if (treeElement.id.toString() == beginId.toString()) {
-        treeElement.parentId = endId;
+        if (treeElement.parentId !== endId && treeElement.id_staff) {
+            treeElement.parentId = endId;
+            // сохраняем в базе
+            this.dds.updateStaffObject(treeElement.id, treeElement.parentId*(-1), treeElement.id_staff).subscribe();
+        }
       }
     });
-
-    //console.log('this.data2', this.data);
-
-    this.data = [...this.data];
     
+    this.dd_result = [...this.dd_result];
   }
 
-
-
-  
+ 
 
 
 
--- src/app/d3-org-chart/d3-org-chart.component.ts
+++ src/app/d3-org-chart/d3-org-chart.component.ts
@@ -64,35 +64,31 @@ const color = '#FFFFFF';
 return    `
          <div draggable="true" tree_id=${node.data.id}
              style="font-family: 'Inter', sans-serif;background-color:${color}; position:absolute;margin-top:-1px; margin-left:-1px;width:${node.width}px;height:${node.height}px;border-radius:10px;border: 1px solid #E4E2E9">
-            <div style="background-color:${color};position:absolute;margin-top:-25px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
-            <img draggable="false" src=" ${
+            <div tree_id=${node.data.id} style="background-color:${color};position:absolute;margin-top:-25px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
+            <img draggable="false" tree_id=${node.data.id} src=" ${
               node.data.photo_name
             }" style="position:absolute;margin-top:-20px;margin-left:${20}px;border-radius:100px;width:40px;height:40px;" />
             
-            <div style="color:#08011E;position:absolute;right:20px;top:17px;font-size:10px;"><i class="fas fa-ellipsis-h"></i></div>
+            <div tree_id=${node.data.id} style="color:#08011E;position:absolute;right:20px;top:17px;font-size:10px;"><i tree_id=${node.data.id} class="fas fa-ellipsis-h"></i></div>
 
-            <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:32px"> ${
+            <div tree_id=${node.data.id} style="font-size:15px;color:#08011E;margin-left:20px;margin-top:32px"> ${
               node.data.name
             } </div>
-            <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
+            <div tree_id=${node.data.id} style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
               node.data.address
             } </div>
 
-            <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
+            <div tree_id=${node.data.id} style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
               node.data.rank
             } </div>
 
           </div>
           `;
 
-
       })
       .render();
   }
 
-  dragStart(event: any) {
-    console.log('dragStart');
-  }
-
+  
 
 }
